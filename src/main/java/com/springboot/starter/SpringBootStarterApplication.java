2169a037acba690d39575f76510f082006cb58c9
Srinath Rayabarapu <srinath.rayabarapu@tesco.com>
2022-04-24 11:28:43 +0530

    Added logback

--- src/main/java/com/springboot/starter/SpringBootStarterApplication.java
+++ src/main/java/com/springboot/starter/SpringBootStarterApplication.java
@@ -1,13 +1,16 @@
 package com.springboot.starter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootStarterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterApplication.class, args);
+		log.info("SpringBoot app started");
 	}
 
 }
